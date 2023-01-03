06b7e9cf1579bf3120d5cf2f124c330fe93081ec
Guillaume FORTAINE <guillaume+github@fortaine.com>
2023-01-03 13:36:49 +0100

    chore: various fixes before publish
    • add CJS build
    • add typesVersions
    • add tsconfig.test.json

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export * as BaseEventSource from './BaseEventSource.js';
 export * as Node from './node/EventSource.js';
 export * as Undici from './undici/EventSource.js';
 
-export { EventSource } from './node/EventSource';
+export { EventSource } from './node/EventSource.js';
 export { eventStreamReader } from './eventStreamReader.js';
